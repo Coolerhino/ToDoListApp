af4d7e3b9aae43e85e0c024c6b163bdd5fb381e0
Emilian Lato <Emilian.Lato@Live.umcs.edu.pl>
2019-01-11 00:57:00 +0100

    Improve controller functionality add rest api to interact with commands

--- ToDoListApp/Controllers/ToDoListController.cs
+++ ToDoListApp/Controllers/ToDoListController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ToDoListApp.Application.ToDoItems.Commands.AddToDoItem;
+using ToDoListApp.Application.ToDoItems.Commands.DeleteToDoItem;
 using ToDoListApp.Application.ToDoItems.Queries;
 using ToDoListApp.Domain.Entities;
 
@@ -16,6 +18,27 @@ namespace ToDoListApp.Controllers
             var items = await Mediator.Send(new GetAllToDoItemsQuery());
                 //var vm = new ToDoItemViewModel{ToDoItems = items};
             return View(items);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await Mediator.Send(new DeleteToDoItemCommand {Id = id});
+            return Ok();
+        }
+
+        public IActionResult Create()
+        {
+            //w ogole zrob brzydko podstawowa funkcjonalnosc i potem taski stad przerzucaj tam
+            //todo return form view for creating
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]CreateToDoItemCommand command)
+        {
+            await Mediator.Send(command);
+            return Ok();
         }
     }
 }
